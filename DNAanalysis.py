5bddf7a5c8b6b15de141f9e13a1775bc2add603b
lncxd <davidlu1111@gmail.com>
2017-07-28 16:36:02 -0500

    Update DNAanalysis.py
    Neater code

--- DNAanalysis.py
+++ DNAanalysis.py
@@ -3,9 +3,10 @@
 def ATGC_content(DNA):  #returns ATGC contents in the form of a dict
 
     ATGCDict = {'A' : 0,
-                'T': 0,
+                'T' : 0,
                 'G' : 0,
                 'C' : 0}
+    
     for nucleotide in DNA:
         if nucleotide == 'A':
             ATGCDict['A'] += 1
@@ -15,6 +16,7 @@ def ATGC_content(DNA):  #returns ATGC contents in the form of a dict
             ATGCDict['G'] += 1
         else:
             ATGCDict['C'] += 1
+            
     return ATGCDict
 
 
