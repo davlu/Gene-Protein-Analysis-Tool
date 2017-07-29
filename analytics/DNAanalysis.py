947e0ae1328f67f8e6a061187e7423760d42426a
lncxd <ricky0zhou@gmail.com>
2017-07-29 15:06:36 -0500

    Added hamming difference function

--- analytics/DNAanalysis.py
+++ analytics/DNAanalysis.py
@@ -40,7 +40,17 @@ def complementary(DNA): #todo: given a DNA sequence, will return a complementary
 
 
 def hamming_differences(DNA1,DNA2): #todo: Returns info on positional/nucleotide differences in 2 DNA strands
-    '''
-    hamming differences:  shatner vs walken
-    '''
-    pass
+    hDiff = []
+    if len(DNA1)>len(DNA2):
+        for i in range(len(DNA1)):
+            DNA2+='X'
+    else:
+        for i in range(len(DNA2)):
+            DNA1+='X'
+    for n1,n2 in list(zip(DNA1,DNA2)):
+        if n1!=n2:
+            locDiff = list(zip(DNA1, DNA2)).index((n1, n2))
+            nucDiff = n1+n2
+            print(str(locDiff)+nucDiff)
+            hDiff.append(str(locDiff)+nucDiff)
+    return hDiff
