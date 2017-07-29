e7b6ed2982e8f067f61660e9aa0aec540a2807ce
pcapel <insert.available.name@gmail.com>
2017-07-28 22:01:36 -0500

    restructured the files to allow for the integration of Click as CLI manager
    added support for a simple string parser and output of the counts for the function added to DNAanalysis.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+	name='GPA_Tool',
+	version='0.0.1',
+	py_modules=['analytics'],
+	install_requires=[
+		'Click',
+	],
+	entry_points='''
+	[console_scripts]
+	gpa-tool=MainAnalysis:main
+	''',
+	)
