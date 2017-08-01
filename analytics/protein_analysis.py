6e47f2a6d27f8c57a3c360b19dcc8a6f75e065a2
karochi1 <30536993+karochi1@users.noreply.github.com>
2017-08-01 10:30:48 +0200

    Update protein_analysis.py
    New features and changes:
    
    New way to load files with GUI (tkinter)
    Compatibility added for other types of files.
    Auto-find start and end of sequence in file
    Added definition of making DNA sequence to mRNA sequence
    More commented code and less nested loops
    
    /Karochi1

--- analytics/protein_analysis.py
+++ analytics/protein_analysis.py
@@ -1,7 +1,7 @@
+# Needed for regex
 import re
 
-fasta_file = input('Type your fasta file name: ')
-
+# Codon table    
 codons = {
     'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
     'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
@@ -21,9 +21,92 @@ codons = {
     'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W'
     }
 
-def dna2AA(fastaFile,codons):
+
+"""Definitions"""
+
+def load_sequence():
+    # Loads file name
+	try:
+		# Try: opening and using tkinter GUI. If any errors, you have to manually type it in
+		import tkinter # GUI module
+		from tkinter import filedialog # Choose file function
+		from tkinter.filedialog import askopenfilename # 
+		root = tkinter.Tk() # Makes a root window
+		root.withdraw() # Withdraws window (since root window is not needed)
+		sequence_file = askopenfilename(parent=root,initialdir=cwd,title='Please select a file') # Opens the GUI to pick a file
+		#PS: A fasta file is not needed. Almost any file with proper structure would work
+	except:
+		sequence_file = input('Unable to use GUI. Type your file name with extension: ')
+
+        
+	"""Note: Multi compatible with different files"""
+			
+	# Opens and reads the file
+	with open(sequence_file,'r') as f:
+		DNA_sequence = f.readlines()
+
+	# Checks where the file start and ends
+	for i in DNA_sequence:
+		match = re.match("^[atgc]*$", i.lower()) # Lower case to improve compatibility
+		if match is None: # If line contains something else than a,t,g or c
+			if start_of_sequence is not None:
+				end_of_sequence = i.index(i) # Previous line is the end
+			pass # Pass if line contains something else
+		
+		else: # If line only contains a,t,g and c
+			if start_of_sequence is None:
+				start_of_sequence = i.index(i) # Previous line must be non-sequence info           
+                
+	# Cuts away other parts
+	DNA_sequence = DNA_sequence[start_of_sequence:end_of_sequence]
+    
+    # Makes the sequence into a string (sequence is at this moment a list)
+    DNA_sequence = [line.replace('\n','') for line in DNA_sequence] # Removes \n from list
+    DNA_sequence = ''.join(DNA_sequence) # Joins list into string 
+    DNA_sequence = DNA_sequence.upper() # Makes the sequence into high case
+    
+    # Return string
+    return DNA_sequence
+    
+
+# Function that determines what RNA sequence is made from a DNA sequence
+def DNA2RNA(DNA_sequence)
+    """Theory: When a gene is read, only one strand is read and transcribed to mRNA.
+    The mRNA that is transcribed is the complimentary of the read strand, the anti-sense strand.
+    However, you have to take in account for the direction of transcription and translation of the strand (5' to 3').
+    The the sense strand (coding strand) has the same sequence as the mRNA but with U instead of T.
+    Simplified solution: The coding strand (the gene) is the same as the mRNA sequence, but with U instead of T."""    
+    
+    mRNA_sequence=DNA_sequence.replace('T','U')
+    
+    return mRNA_sequence
+
+
+
+
+
+
+
+"""
+The definition "load_sequence()" can be used to store as a string.
+Example:
+
+DNA_sequence = load_sequence() <-- opens up a window and you choose a file that automatically loads it as string
+
+mRNA_sequence = DNA2RNA(DNA_sequence) <-- Will return string as usual
+
+mRNA_sequence = DNA2RNA(load_sequence()) <-- Will open up a window, lets you choose a file and then returns string
+
+"""
+
+
+"""Old code. Saving for lncxd. Note: I have renamed "fastaFile" to "sequence_file""""
+
+"""
+# Function that determines what protein is made from a DNA sequence
+def DNA2AA(fastaFile,codons):
     AAsequence = ''
-    with open(fastaFile) as fileObj:
+    with open(fastaFile,'r') as fileObj: # Added 'r' to the open
         for line in fileObj:
             line.strip()
         cont = fileObj.read()
@@ -34,3 +117,6 @@ def dna2AA(fastaFile,codons):
             seq = cont[i:i+3]
             AAsequence += codons[seq]
     return AAsequence
+"""
+
+    
